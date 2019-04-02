ca2000e0ba7aab39e96761e4e83b14b58f72b713
nikolasekiw <nnikolasekiw@gmail.com>
2019-04-02 11:35:58 +0200

    la til et par ting i klassene

--- src/main/java/org/openjfx/Base/Arrangement.java
+++ src/main/java/org/openjfx/Base/Arrangement.java
@@ -1,5 +1,7 @@
 package org.openjfx.Base;
 
+import java.util.Date;
+
 public class Arrangement {
     private Kontaktperson kontaktperson;
     private String arrangementNavn;
@@ -8,10 +10,43 @@ public class Arrangement {
 
     public Arrangement (Kontaktperson kontaktperson, String arrangementNavn, int billettpris, Date tidspunkt){
         this.kontaktperson = kontaktperson;
-
-
+        this.arrangementNavn = arrangementNavn;
+        this.billettpris = billettpris;
+        this.tidspunkt = tidspunkt;
     }
 
-    private String program(Kontaktperson kontaktperson, String arrangementNavn, int billettPris, Date tidspunkt);
+    public Kontaktperson getKontaktperson() {
+        return kontaktperson;
+    }
+
+    public String getArrangementNavn() {
+        return arrangementNavn;
+    }
+
+    public int getBillettpris() {
+        return billettpris;
+    }
+
+    public Date getTidspunkt() {
+        return tidspunkt;
+    }
+
+    public void setKontaktperson(Kontaktperson kontaktperson) {
+        this.kontaktperson = kontaktperson;
+    }
+
+    public void setArrangementNavn(String arrangementNavn) {
+        this.arrangementNavn = arrangementNavn;
+    }
+
+    public void setBillettpris(int billettpris) {
+        this.billettpris = billettpris;
+    }
+
+    public void setTidspunkt(Date tidspunkt) {
+        this.tidspunkt = tidspunkt;
+    }
+
+    //private String program(Kontaktperson kontaktperson, String arrangementNavn, int billettPris, Date tidspunkt){}
 
 }
--- src/main/java/org/openjfx/Base/ArrangementRegister.java
+++ src/main/java/org/openjfx/Base/ArrangementRegister.java
@@ -1,4 +1,17 @@
 package org.openjfx.Base;
 
+import java.util.ArrayList;
+
 public class ArrangementRegister {
+
+    ArrayList<Arrangement> arrangementer  = new ArrayList<>();
+
+
+    private void registrerArrangement(Arrangement etArrangement){}
+
+    private void visAlleArrangementer(){}
+
+    //egne metoder for å vise arrangementer kun i kinosal/teatersal/foredragssal/konsertsal?
+    //metoder for å slette/endre arrangementer?
+
 }
