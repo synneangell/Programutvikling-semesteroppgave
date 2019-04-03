138209c09c2533c2e8ad02da8fbbd67e9ba8f3ad
martineea <martine.holmberg@gmail.com>
2019-04-03 16:19:32 +0200

    Lagt til ny javaklasse: LokalRegister og lagt inn lokaler

--- src/main/java/org/openjfx/base/LokalRegister.java
+++ src/main/java/org/openjfx/base/LokalRegister.java
@@ -2,5 +2,11 @@ package org.openjfx.base;
 
 public class LokalRegister {
 
-    // legge til lokaltype her
+    Lokale Kinosal = new Lokale (200);
+
+    Lokale Teatersal = new Lokale (250);
+
+    Lokale Konsertsal = new Lokale (400);
+
+    Lokale Foredragssal = new Lokale (100);
 }
--- src/main/java/org/openjfx/base/Lokale.java
+++ src/main/java/org/openjfx/base/Lokale.java
@@ -2,4 +2,8 @@ package org.openjfx.base;
 
 public class Lokale {
     private int plasser;
+
+    Lokale (int plasser) {
+        this.plasser = plasser;
+    }
 }
