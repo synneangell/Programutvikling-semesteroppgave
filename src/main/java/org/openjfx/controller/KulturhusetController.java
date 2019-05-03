597fa87f64877b84452600af50d0d2f3db2c8d63
Synne Angell <synnemb@yahoo.no>
2019-05-03 12:12:24 +0200

    Ordnet opp i controllerne

---
+++ src/main/java/org/openjfx/controller/KulturhusetController.java
@@ -0,0 +1,86 @@
+package org.openjfx.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.FileChooser;
+import org.openjfx.controller.hjelpeklasser.TrådLeserStarter;
+import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+
+public class KulturhusetController {
+
+    @FXML
+    private AnchorPane rootPane;
+
+    @FXML
+    private Label lblLasterInn;
+
+    @FXML
+    private Button btnLesInnBestilling;
+
+    @FXML
+    void endreArrangement(ActionEvent event) throws IOException {
+        launchEndreArrangement();
+    }
+
+    @FXML
+    void opprettBillett (ActionEvent event) throws IOException {
+        launchOpprettBillett();
+    }
+
+    @FXML
+    void opprettArrangement (ActionEvent event) throws IOException {
+        launchOpprettArrangement();
+    }
+
+    @FXML
+    void seBilletter (ActionEvent event) throws IOException {
+        launchSeBilletter();
+    }
+
+    private void threadFerdig() {
+        btnLesInnBestilling.setDisable(false);
+        lblLasterInn.setText("Filen er lastet inn.");
+    }
+
+    @FXML
+    void lesInnBestilling (ActionEvent event) {
+        FileChooser fc = new FileChooser();
+        fc.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("JOBJ og CSV filer", "*.csv", "*.jobj"));
+        File markertFil = fc.showOpenDialog(null);
+
+        try {
+            TrådLeserStarter.startLeser(markertFil);
+            threadFerdig();
+        }
+        catch (ExecutionException | InterruptedException e) {
+            lblLasterInn.setText("Filen ble ikke lastet inn.");
+            e.printStackTrace();
+        }
+    }
+
+    private void launchOpprettBillett() throws IOException {
+        AnchorPane pane = FXMLLoader.load(getClass().getResource("/org/openjfx/opprettBillett.fxml"));
+        rootPane.getChildren().setAll(pane);
+    }
+
+    private void launchOpprettArrangement() throws IOException {
+        AnchorPane pane2 = FXMLLoader.load(getClass().getResource("/org/openjfx/opprettArrangement.fxml"));
+        rootPane.getChildren().setAll(pane2);
+    }
+
+    private void launchEndreArrangement() throws IOException {
+        AnchorPane pane3 = FXMLLoader.load(getClass().getResource("/org/openjfx/endreArrangement.fxml"));
+        rootPane.getChildren().setAll(pane3);
+    }
+
+    private void launchSeBilletter() throws IOException {
+        AnchorPane pane3 = FXMLLoader.load(getClass().getResource("/org/openjfx/seBilletter.fxml"));
+        rootPane.getChildren().setAll(pane3);
+    }
+}
--- src/main/java/org/openjfx/controller/OpprettBillettController.java
+++ src/main/java/org/openjfx/controller/OpprettBillettController.java
@@ -1,87 +1,143 @@
 package org.openjfx.controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
-import javafx.stage.FileChooser;
-import org.openjfx.controller.hjelpeklasser.TrådLeser;
-import org.openjfx.controller.hjelpeklasser.TrådLeserStarter;
-import java.io.File;
+import javafx.stage.Stage;
+import org.openjfx.Filbehandling.*;
+import org.openjfx.base.*;
+import org.openjfx.controller.hjelpeklasser.*;
 import java.io.IOException;
-import java.util.concurrent.ExecutionException;
+import java.text.ParseException;
+import java.util.ArrayList;
 
 public class OpprettBillettController {
 
-    @FXML
-    private AnchorPane rootPane;
+    ObservableList<String> AntallBilletter = FXCollections.observableArrayList("1", "2", "3", "4", "5", "6", "7", "8", "9", "10");
+    ObservableList<String> filtyper = FXCollections.observableArrayList(".jobj", ".csv");
 
     @FXML
-    private Label lblLasterInn;
+    private TextField txtNavn, txtTelefonnummer, txtEmail;
 
     @FXML
-    private Button btnLesInnBestilling;
+    private ComboBox chboxVelgAntall;
 
     @FXML
-    void endreArrangement(ActionEvent event) throws IOException {
-        launchEndreArrangement();
+    private ComboBox lagreTilFilBox;
+
+    @FXML
+    private Button btnAvslutt;
+
+    @FXML
+    private AnchorPane rootKjøpBillett;
+
+    @FXML
+    private TableView<Arrangement> tableView;
+
+    @FXML
+    private TableColumn<Arrangement, String> ArrangementTypeColumn;
+
+    @FXML
+    private TableColumn<Arrangement, String> ArrangementNavnColumn;
+
+    @FXML
+    private TableColumn<Arrangement, String> KlokkeslettColumn;
+
+    @FXML
+    private TableColumn<Arrangement, String> DatoColumn;
+
+    @FXML
+    private TableColumn<Arrangement, Integer> PrisColumn;
+
+    @FXML
+    public void initialize() {
+        //Oppretter tabellen som viser arrangementene:
+        ArrangementTypeColumn.setCellValueFactory(new PropertyValueFactory<Arrangement, String>("typeArrangement"));
+        ArrangementNavnColumn.setCellValueFactory(new PropertyValueFactory<Arrangement, String>("arrangementNavn"));
+        KlokkeslettColumn.setCellValueFactory(new PropertyValueFactory<Arrangement, String>("klokkeslett"));
+        DatoColumn.setCellValueFactory(new PropertyValueFactory<Arrangement, String>("dato"));
+        PrisColumn.setCellValueFactory(new PropertyValueFactory<Arrangement, Integer>("billettpris"));
+
+        AlleArrangementer alleArrangementer = AlleArrangementer.getSingelton();
+        tableView.setItems(alleArrangementer.getArrangementer());
+
+        chboxVelgAntall.setItems(AntallBilletter);
+        chboxVelgAntall.setValue("1");
+
+        lagreTilFilBox.setItems(filtyper);
+        lagreTilFilBox.setValue(".csv");
     }
 
     @FXML
-    void kjøpBillett (ActionEvent event) throws IOException {
-        launchKjøpBillett();
-    }
+     void fullførBestilling(ActionEvent event) throws ParseException, IOException {
 
-    @FXML
-    void bookLokale (ActionEvent event) throws IOException {
-        launchBookLokale();
-    }
+        if (!txtNavn.getText().isEmpty() && !txtTelefonnummer.getText().isEmpty() && !txtEmail.getText().isEmpty()) {
+                try {
+                    if (SjekkOmGyldig.sjekkKunBokstaver(txtNavn.getText()) && SjekkOmGyldig.sjekkGyldigTlfNr(txtTelefonnummer.getText()) &&
+                            SjekkOmGyldig.sjekkGyldigEmail(txtEmail.getText())) {
 
-    @FXML
-    void seBilletter (ActionEvent event) throws IOException {
-        launchSeBilletter();
-    }
+                        Arrangement etArrangement = tableView.getSelectionModel().getSelectedItem();
 
-    private void threadFerdig() {
-        btnLesInnBestilling.setDisable(false);
-        lblLasterInn.setText("Filen er lastet inn.");
-    }
+                        int antallBilletter = Integer.valueOf((String) chboxVelgAntall.getValue());
 
-    @FXML
-    void lesInnBestilling (ActionEvent event) {
-        FileChooser fc = new FileChooser();
-        fc.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("JOBJ og CSV filer", "*.csv", "*.jobj"));
-        File markertFil = fc.showOpenDialog(null);
+                        Kjøper enKjøper = new Kjøper(txtNavn.getText(), txtTelefonnummer.getText(), txtEmail.getText());
+                        String ut = etArrangement.leggTilBillett(enKjøper, antallBilletter);
 
-        try {
-            TrådLeserStarter.startLeser(markertFil);
-            threadFerdig();
+                        AlertBoks.generateAlert(ut);
+                    }
+                } catch (InvalidTekstException e) {
+                    FeilhåndteringsAlertBoks.generateAlert("Det er brukt tall der det kun skal være tekst. ");
+                } catch (InvalidTelefonnummerException e) {
+                    FeilhåndteringsAlertBoks.generateAlert("Ikke gyldig telefonnummer skrevet inn. ");
+                } catch (InvalidEmailException e) {
+                    FeilhåndteringsAlertBoks.generateAlert("Ikke gyldig email skrevet inn.");
+                }
+            }
         }
-        catch (ExecutionException | InterruptedException e) {
-            lblLasterInn.setText("Filen ble ikke lastet inn.");
-            e.printStackTrace();
+
+   @FXML
+    void Lagre (ActionEvent event) throws IOException {
+    String filtype = lagreTilFilBox.getValue().toString();
+        AlleArrangementer alleArrangementer = AlleArrangementer.getSingelton();
+
+        ArrayList<Billett> lagreBilletterTilFil = new ArrayList<>();
+
+        for(Arrangement etArrangement : alleArrangementer.getArrangementer()){
+            for(Billett enBillett : etArrangement.getBilletter()){
+                if(enBillett.getKjøper() != null) {
+                    lagreBilletterTilFil.add(enBillett);
+                }
+            }
+        }
+
+        if(filtype.equals(".csv")){
+            SkriveCsvFil skriveTilCsvFil = new SkriveCsvFil();
+            skriveTilCsvFil.skriveTilFil("billett.csv", lagreBilletterTilFil);
+        }
+        else if(filtype.equals(".jobj")){
+            SkriveJobjFil skriveTilJobjFil = new SkriveJobjFil();
+            skriveTilJobjFil.skriveTilFil("billett.jobj",lagreBilletterTilFil);
         }
     }
 
-    private void launchKjøpBillett() throws IOException {
-        AnchorPane pane = FXMLLoader.load(getClass().getResource("/org/openjfx/OpprettBillett.fxml"));
-        rootPane.getChildren().setAll(pane);
-    }
+      private void avsluttProgram() {
+            Stage stage = (Stage) btnAvslutt.getScene().getWindow();
+            stage.close();
+      }
 
-    private void launchBookLokale() throws IOException {
-        AnchorPane pane2 = FXMLLoader.load(getClass().getResource("/org/openjfx/OpprettArrangement.fxml"));
-        rootPane.getChildren().setAll(pane2);
-    }
+      @FXML
+      private void Avslutt (ActionEvent event){
+            avsluttProgram();
+        }
 
-    private void launchEndreArrangement() throws IOException {
-        AnchorPane pane3 = FXMLLoader.load(getClass().getResource("/org/openjfx/endreArrangement.fxml"));
-        rootPane.getChildren().setAll(pane3);
-    }
-
-    private void launchSeBilletter() throws IOException {
-        AnchorPane pane3 = FXMLLoader.load(getClass().getResource("/org/openjfx/seBilletter.fxml"));
-        rootPane.getChildren().setAll(pane3);
-    }
+      @FXML
+      private void Tilbake (ActionEvent event) throws IOException {
+            AnchorPane pane = FXMLLoader.load(getClass().getResource("/org/openjfx/kulturhuset.fxml"));
+            rootKjøpBillett.getChildren().setAll(pane);
+      }
 }
