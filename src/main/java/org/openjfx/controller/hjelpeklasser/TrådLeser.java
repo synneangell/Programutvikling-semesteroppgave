ff65150797ae13a6820dff25c7515e51de156abb
nikolasekiw <nnikolasekiw@gmail.com>
2019-05-03 12:59:19 +0200

    Tråd

--- src/main/java/org/openjfx/controller/hjelpeklasser/TrådLeser.java
+++ src/main/java/org/openjfx/controller/hjelpeklasser/TrådLeser.java
@@ -4,6 +4,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import org.openjfx.Filbehandling.ArrangementCsvLeser;
 import org.openjfx.Filbehandling.BillettCsvLeser;
+import org.openjfx.Filbehandling.LeseFil;
 import org.openjfx.Filbehandling.LeseJobjFil;
 import org.openjfx.base.AlleArrangementer;
 import java.io.File;
@@ -20,7 +21,7 @@ public class TrådLeser implements Callable<ObservableList<Object>> {
 
     @Override
     public ObservableList<Object> call() throws InvalidBillettFormatException, IOException, InvalidFilDataException, InterruptedException {
-
+        Thread.sleep(3000);
         return read();
     }
 
@@ -33,30 +34,36 @@ public class TrådLeser implements Callable<ObservableList<Object>> {
 
             String[] array = markertFil.getName().split("\\.");
             if(array.length > 1) {
+                LeseFil filleser = null;
                 if (array[1].equals("csv")){
+
                     if(array[0].equals("billett")){
                         BillettCsvLeser lesBillettFraCsvFil = new BillettCsvLeser();
+                        filleser = new BillettCsvLeser();
                         alleArrangementer.LeggBillettFraFilTilArrangement(lesBillettFraCsvFil.leseFraFil(markertFil.getPath()));
-                        // returVerdi =
+                        returVerdi = FXCollections.observableArrayList(lesBillettFraCsvFil.leseFraFil(markertFil.getPath()));
+
                     }
                     if(array[0].equals("arrangement")){
                         ArrangementCsvLeser lesArrangementFraCsvFil = new ArrangementCsvLeser();
                         alleArrangementer.gjørOmTilObservableList(lesArrangementFraCsvFil.leseFraFil(markertFil.getPath()));
-                        //returVerdi = alleArrangementer.getArrangementer();
+                        returVerdi = FXCollections.observableArrayList(lesArrangementFraCsvFil.leseFraFil(markertFil.getPath()));
                     }
                 }
                 if (array[1].equals("jobj")){
                     if(array[0].equals("billett")){
                         LeseJobjFil leseDataFraJobjFil = new LeseJobjFil();
                         alleArrangementer.LeggBillettFraFilTilArrangement(leseDataFraJobjFil.leseFraFil(markertFil.getPath()));
-                        //returVerdi = ??
+                        returVerdi = FXCollections.observableArrayList(leseDataFraJobjFil.leseFraFil(markertFil.getPath()));
                     }
                     if(array[0].equals("arrangement")){
                         LeseJobjFil leseDataFraJobjFil = new LeseJobjFil();
                         alleArrangementer.gjørOmTilObservableList(leseDataFraJobjFil.leseFraFil(markertFil.getPath()));
-                        //returVerdi = ??
+                        returVerdi = FXCollections.observableArrayList(leseDataFraJobjFil.leseFraFil(markertFil.getPath()));
                     }
                 }
+                if (filleser!=null)
+                returVerdi = FXCollections.observableArrayList(filleser.leseFraFil(markertFil.getPath()));
             }
         } else {
             throw new InvalidFilDataException("Filen er ikke gyldig");
